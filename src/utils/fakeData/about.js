fa44d3ed35dce2b65284d7d65f9830fde7c917a1
deceser <aufhaummtobsc@gmail.com>
2023-07-13 16:34:40 +0300

    Created | Added About page

--- src/utils/fakeData/about.js
+++ src/utils/fakeData/about.js
@@ -1,5 +1,5 @@
 export const dataAbout = {
   title: "About my self",
   aboutme:
-    "Hi there! My name is Denis and I'm a talented Full-Stack developer with over 2 years of commercial experience.In my work, I’m following best development practices and pay close attention to code quality. I am currently available for full-time or part-time work, depending on the need. I'm flexible at work and can adapt to the required time zone.Beyond my technical capabilities, I possess excellent communication skills in both verbal and written English. I am confident in my skills and ready to help you turn your ideas into successful applications. Thanks!",
+    "Hi there! My name is Denis and I'm a talented Front-end developer with over 2 years of commercial experience.In my work, I’m following best development practices and pay close attention to code quality. I am currently available for full-time or part-time work, depending on the need. I'm flexible at work and can adapt to the required time zone.Beyond my technical capabilities, I possess excellent communication skills in both verbal and written English. I am confident in my skills and ready to help you turn your ideas into successful applications. Thanks!",
 };
