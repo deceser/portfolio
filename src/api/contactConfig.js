7217ec5a44021355996ac6278ceb4183f682d4ef
deceser <aufhaummtobsc@gmail.com>
2023-07-17 09:24:20 +0300

    Created | Added Contact page

---
+++ src/api/contactConfig.js
@@ -0,0 +1,8 @@
+export const contactConfig = {
+  YOUR_EMAIL: import.meta.env.VITE_REACT_APP_YOUR_EMAIL,
+  YOUR_FONE: import.meta.env.VITE_REACT_APP_YOUR_FONE,
+  YOUR_SERVICE_ID: import.meta.env.VITE_REACT_APP_YOUR_SERVICE_ID,
+  YOUR_TEMPLATE_ID: import.meta.env.VITE_REACT_APP_YOUR_TEMPLATE_ID,
+  YOUR_USER_ID: import.meta.env.VITE_REACT_APP_YOUR_USER_ID,
+  description: "Contact me for cooperation.",
+};
