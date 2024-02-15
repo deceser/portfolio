897f2cb81995aafce925fb33afea7196cf089196
Denys <101974867+deceser@users.noreply.github.com>
2024-02-15 12:31:54 +0200

    Update social.js

--- src/utils/fakeData/social.js
+++ src/utils/fakeData/social.js
@@ -2,5 +2,5 @@ export const socialProfils = {
   github: "https://github.com/deceser",
   instagram: "https://www.instagram.com/petitemeandyou/",
   linkedin: "https://www.linkedin.com/in/denys-bezverkhyi-80a79b266/",
-  telegram: "https://t.me/deceser",
+//  telegram: "https://t.me/deceser",
 };
