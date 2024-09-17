38b38cb03cb67ed4e4412d6a6018ac7f337da569
deceser <aufhaummtobsc@gmail.com>
2024-09-17 12:07:42 +0300

    commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -45,14 +45,14 @@ const Home = () => {
                     <div className="ring three"></div>
                   </div>
                 </NavLink>
-                <NavLink to="contact">
+                {/* <NavLink to="contact">
                   <div id="button_h" className="ac_btn btn">
                     Contact Me
                     <div className="ring one"></div>
                     <div className="ring two"></div>
                     <div className="ring three"></div>
                   </div>
-                </NavLink>
+                </NavLink> */}
                 <NavLink to="about">
                   <div id="button_p" className="ac_btn btn">
                     About Me
